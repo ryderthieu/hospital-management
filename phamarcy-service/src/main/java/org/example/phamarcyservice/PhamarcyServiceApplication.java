47097179e76cf6913697d77e534bbb30987b4f7b
ryderthieu <ryderthieu@gmai.com>
2025-04-06 10:13:29 +0700

    init project

---
+++ phamarcy-service/src/main/java/org/example/phamarcyservice/PhamarcyServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.phamarcyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamarcyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhamarcyServiceApplication.class, args);
+    }
+
+}
---
+++ phamarcy-service/src/test/java/org/example/phamarcyservice/PhamarcyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.phamarcyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamarcyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
