e85a726afa60b76baad433341af261c274f111a6
tdpnhi <trandophuongnhi111@gmail.com>
2025-05-01 08:34:56 +0700

    update appointment + appointment note repository

---
+++ backend/appointment-service/src/main/java/org/example/appointmentservice/repository/AppointmentRepository.java
@@ -0,0 +1,9 @@
+package org.example.appointmentservice.repository;
+
+import org.example.appointmentservice.entity.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
+}
