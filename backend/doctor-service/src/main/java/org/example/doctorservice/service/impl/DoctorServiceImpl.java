9559c01782cafcdf0e12bf9c38de33288245c376
tdpnhi <trandophuongnhi111@gmail.com>
2025-05-03 09:52:49 +0700

    update doctor service

---
+++ backend/doctor-service/src/main/java/org/example/doctorservice/service/impl/DoctorServiceImpl.java
@@ -0,0 +1,107 @@
+package org.example.doctorservice.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.example.doctorservice.dto.DoctorDto;
+import org.example.doctorservice.entity.Department;
+import org.example.doctorservice.entity.Doctor;
+import org.example.doctorservice.repository.DepartmentRepository;
+import org.example.doctorservice.repository.DoctorRepository;
+import org.example.doctorservice.service.DoctorService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class DoctorServiceImpl implements DoctorService {
+    private final DoctorRepository doctorRepository;
+    private final DepartmentRepository departmentRepository;
+
+    @Override
+    public List<DoctorDto> getAllDoctors() {
+        return doctorRepository
+                .findAll()
+                .stream()
+                .map(DoctorDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public DoctorDto getDoctorById(Integer doctorId) {
+        Doctor doctor = doctorRepository.findById(doctorId)
+                .orElseThrow(() -> new RuntimeException("Không tìm thấy bác sĩ với ID:" + doctorId));
+        return new DoctorDto(doctor);
+    }
+
+    @Override
+    public DoctorDto createDoctor(DoctorDto doctorDto) {
+        Department department = departmentRepository.findById(doctorDto.getDepartmentId())
+                .orElseThrow(() -> new RuntimeException("Phòng không được tìm thấy"));
+        Doctor doctor = Doctor.builder()
+                .userId(doctorDto.getUserId())
+                .identityNumber(doctorDto.getIdentityNumber())
+                .fullName(doctorDto.getFullName())
+                .birthday(doctorDto.getBirthday())
+                .gender(doctorDto.getGender())
+                .address(doctorDto.getAddress())
+                .academicDegree(doctorDto.getAcademicDegree())
+                .specialization(doctorDto.getSpecialization())
+                .type(doctorDto.getType())
+                .department(department)
+                .build();
+
+        Doctor createdDoctor = doctorRepository.save(doctor);
+        return new DoctorDto(createdDoctor);
+    }
+
+    @Override
+    public DoctorDto updateDoctor(Integer doctorId, DoctorDto doctorDto) {
+        Doctor doctor = doctorRepository.findById(doctorId)
+                .orElseThrow(() -> new RuntimeException("Không tìm thấy bác sĩ với ID:" + doctorId));
+
+        if (!doctor.getIdentityNumber().equals(doctor.getIdentityNumber())) {
+            doctor.setIdentityNumber(doctor.getIdentityNumber());
+        }
+
+        doctor.setFullName(doctorDto.getFullName());
+        doctor.setBirthday(doctorDto.getBirthday());
+        doctor.setGender(doctorDto.getGender());
+        doctor.setAddress(doctorDto.getAddress());
+        doctor.setAcademicDegree(doctorDto.getAcademicDegree());
+        doctor.setSpecialization(doctorDto.getSpecialization());
+        doctor.setType(doctorDto.getType());
+
+        Doctor updatedDoctor = doctorRepository.save(doctor);
+        return new DoctorDto(updatedDoctor);
+    }
+
+    @Override
+    public void deleteDoctor(Integer doctorId) {
+        Doctor doctor = doctorRepository.findById(doctorId)
+                .orElseThrow(() -> new RuntimeException("Không tìm thấy bác sĩ với ID:" + doctorId));
+
+        doctorRepository.delete(doctor);
+    }
+
+    @Override
+    public Optional<DoctorDto> findByIdentityNumber(String identityNumber) {
+        if (identityNumber != null && !identityNumber.isEmpty()) {
+            return doctorRepository
+                    .findByIdentityNumber(identityNumber)
+                    .map(DoctorDto::new);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<DoctorDto> filterDoctors(Doctor.Gender gender,
+                                             Doctor.AcademicDegree academicDegree,
+                                             String specialization,
+                                             Doctor.Type type) {
+        return doctorRepository.filterDoctors(gender, academicDegree, specialization, type)
+                .map(DoctorDto::new);
+    }
+}
+
