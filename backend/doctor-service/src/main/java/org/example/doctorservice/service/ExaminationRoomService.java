2d8427cbf0d3ffdd7f4df389484f634994a98355
tdpnhi <trandophuongnhi111@gmail.com>
2025-05-03 09:25:58 +0700

    update examination room service

--- backend/doctor-service/src/main/java/org/example/doctorservice/service/ExaminationRoomService.java
+++ backend/doctor-service/src/main/java/org/example/doctorservice/service/ExaminationRoomService.java
@@ -1,4 +1,20 @@
 package org.example.doctorservice.service;
 
+import org.example.doctorservice.dto.ExaminationRoomDto;
+import org.example.doctorservice.entity.ExaminationRoom;
+
+import java.util.List;
+
 public interface ExaminationRoomService {
+    ExaminationRoomDto getExaminationRoomById(Integer roomId);
+
+    List<ExaminationRoomDto> getAllExaminationRooms();
+
+    ExaminationRoomDto createExaminationRoom(ExaminationRoomDto examinationRoomDto);
+
+    ExaminationRoomDto updateExaminationRoom(Integer roomId, ExaminationRoomDto examinationRoomDto);
+
+    void deleteExaminationRoom(Integer roomId);
+
+    List<ExaminationRoomDto> filterRooms(ExaminationRoom.Type type, String building, Integer floor);
 }
---
+++ backend/doctor-service/src/main/java/org/example/doctorservice/service/impl/ExaminationRoomServiceImpl.java
@@ -0,0 +1,90 @@
+package org.example.doctorservice.service.impl;
+
+import lombok.AllArgsConstructor;
+import org.example.doctorservice.dto.ExaminationRoomDto;
+import org.example.doctorservice.entity.Department;
+import org.example.doctorservice.entity.ExaminationRoom;
+import org.example.doctorservice.repository.DepartmentRepository;
+import org.example.doctorservice.repository.ExaminationRoomRepository;
+import org.example.doctorservice.service.ExaminationRoomService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class ExaminationRoomServiceImpl implements ExaminationRoomService {
+    private final ExaminationRoomRepository examinationRoomRepository;
+    private final DepartmentRepository departmentRepository;
+
+    @Override
+    public ExaminationRoomDto getExaminationRoomById(Integer roomId) {
+        ExaminationRoom examinationRoom = examinationRoomRepository.findById(roomId)
+                .orElseThrow(() -> new RuntimeException("Không tìm thấy phòng với ID: " + roomId));
+
+        return new ExaminationRoomDto(examinationRoom);
+    }
+
+    @Override
+    public List<ExaminationRoomDto> getAllExaminationRooms() {
+        List<ExaminationRoom> rooms = examinationRoomRepository.findAll();
+        return rooms
+                .stream()
+                .map(ExaminationRoomDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public ExaminationRoomDto createExaminationRoom(ExaminationRoomDto examinationRoomDto) {
+        Department department = departmentRepository.findById(examinationRoomDto.getDepartmentId())
+                .orElseThrow(() -> new RuntimeException("Khoa không được tìm thấy"));
+        ExaminationRoom examinationRoom = ExaminationRoom.builder()
+                .department(department)
+                .type(examinationRoomDto.getType())
+                .building(examinationRoomDto.getBuilding())
+                .floor(examinationRoomDto.getFloor())
+                .note(examinationRoomDto.getNote())
+                .build();
+
+        ExaminationRoom savedExaminationRoom = examinationRoomRepository.save(examinationRoom);
+        return new ExaminationRoomDto(savedExaminationRoom);
+    }
+
+    @Override
+    public ExaminationRoomDto updateExaminationRoom(Integer roomId, ExaminationRoomDto examinationRoomDto) {
+        ExaminationRoom examinationRoom = examinationRoomRepository.findById(roomId)
+                .orElseThrow(() -> new RuntimeException("Không tìm thấy phòng với ID: " + roomId));
+
+        if (examinationRoomDto.getDepartmentId() != null) {
+            Department department = departmentRepository.findById(examinationRoomDto.getDepartmentId())
+                    .orElseThrow(() -> new RuntimeException("Khoa không được tìm thấy"));
+            examinationRoom.setDepartment(department);
+        }
+
+        examinationRoom.setType(examinationRoomDto.getType());
+        examinationRoom.setBuilding(examinationRoomDto.getBuilding());
+        examinationRoom.setFloor(examinationRoomDto.getFloor());
+        examinationRoom.setNote(examinationRoomDto.getNote());
+
+        ExaminationRoom updatedExaminationRoom = examinationRoomRepository.save(examinationRoom);
+        return new ExaminationRoomDto(updatedExaminationRoom);
+    }
+
+    @Override
+    public void deleteExaminationRoom(Integer roomId) {
+        ExaminationRoom examinationRoom = examinationRoomRepository.findById(roomId)
+                .orElseThrow(() -> new RuntimeException("Không tìm thấy phòng với ID: " + roomId));
+
+        examinationRoomRepository.delete(examinationRoom);
+    }
+
+    @Override
+    public List<ExaminationRoomDto> filterRooms(ExaminationRoom.Type type, String building, Integer floor) {
+        List<ExaminationRoom> rooms = examinationRoomRepository.findRoomsByFilters(type, building, floor);
+        return rooms
+                .stream()
+                .map(ExaminationRoomDto::new)
+                .collect(Collectors.toList());
+    }
+}
