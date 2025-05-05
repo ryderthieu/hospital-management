b8f283f28d40e973d719b72dc416eac691597feb
tdpnhi <trandophuongnhi111@gmail.com>
2025-05-06 01:15:10 +0700

    update api gateway for patient service

--- backend/patient-service/src/main/java/org/example/patientservice/controller/PatientController.java
+++ backend/patient-service/src/main/java/org/example/patientservice/controller/PatientController.java
@@ -6,6 +6,7 @@ import org.example.patientservice.dto.PatientDto;
 import org.example.patientservice.service.PatientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,32 +19,38 @@ public class PatientController {
 
     private final PatientService patientService;
 
+    @PreAuthorize("hasAnyRole('RECEPTIONIST', 'ADMIN')")
     @PostMapping
     public ResponseEntity<PatientDto> createPatient(@RequestBody @Valid PatientDto patientDto) {
         return ResponseEntity.ok(patientService.createPatient(patientDto));
     }
 
+    @PreAuthorize("hasAnyRole('RECEPTIONIST', 'ADMIN', 'DOCTOR')")
     @GetMapping
     public ResponseEntity<List<PatientDto>> getAllPatients() {
         return ResponseEntity.ok(patientService.getAllPatients());
     }
 
+    @PreAuthorize("hasAnyRole('RECEPTIONIST', 'ADMIN', 'DOCTOR')")
     @GetMapping("/{patientId}")
     public ResponseEntity<PatientDto> getPatientById(@PathVariable Integer patientId) {
         return ResponseEntity.ok(patientService.getPatientById(patientId));
     }
 
+    @PreAuthorize("hasAnyRole('RECEPTIONIST', 'ADMIN')")
     @PutMapping("/{patientId}")
     public ResponseEntity<PatientDto> updatePatient(@PathVariable Integer patientId, @RequestBody @Valid PatientDto patientDto) {
         return ResponseEntity.ok(patientService.updatePatient(patientId, patientDto));
     }
 
+    @PreAuthorize("hasAnyRole('RECEPTIONIST', 'ADMIN')")
     @DeleteMapping("/{patientId}")
     public ResponseEntity<String> deletePatient(@PathVariable Integer patientId) {
         patientService.deletePatient(patientId);
         return ResponseEntity.ok("Bệnh nhân được xóa thành công");
     }
 
+    @PreAuthorize("hasAnyRole('RECEPTIONIST', 'ADMIN', 'DOCTOR')")
     @GetMapping("/search")
     public ResponseEntity<Optional<PatientDto>> searchPatient(@RequestParam(required = false) String identityNumber,
                                                               @RequestParam(required = false) String insuranceNumber,
