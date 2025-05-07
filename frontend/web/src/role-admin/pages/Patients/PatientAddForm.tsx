6485e1e6834d05bbdff5f81b02857561d062eb34
anhthotn <22521424@gm.uit.edu.vn>
2025-05-07 08:55:45 +0700

    update addpatient
    # Conflicts:
    #	frontend/web/src/role-admin/pages/Patients/AddPatient.tsx

--- frontend/web/src/role-admin/pages/Patients/PatientAddForm.tsx
+++ frontend/web/src/role-admin/pages/Patients/PatientAddForm.tsx
@@ -137,7 +137,6 @@ export default function PatientAddForm() {
         </form>
       </div>
 
-      {/* Account Information Section */}
       <div className="bg-white rounded-lg border border-gray-200 p-6">
         <h2 className="text-xl font-bold text-red-600 mb-6">Thông tin tài khoản</h2>
 
